43832505fd4de0f6383f551d1c4a1e3f494dc2a2
Mxguinho <joaohartung@gmail.com>
2026-05-07 16:42:02 -0300

    Jogo de adivinha

---
+++ Java/JogoAdivinha/Main.java
@@ -0,0 +1,26 @@
+// Jogo de Adivinhação: O computador escolhe um número aleatório e o usuário tenta adivinhar. 
+// O loop só para quando o usuário acerta, dando dicas de "maior" ou "menor" a cada tentativa.
+
+package JogoAdivivnha;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner s = new Scanner(System.in);
+        int numeroAleatorio, certo = 0, tentativa;
+        while(certo != 1) {
+        numeroAleatorio = random.nextInt(26);    
+        System.out.print("Tente imaginar o número que estou pensando: ");
+        tentativa = s.nextInt();
+        if(tentativa == numeroAleatorio) {
+            certo = 1;
+            System.out.println("acertou!!!");
+            break;
+        }
+        System.out.println("Errou!!! O que eu estava pensando era " + numeroAleatorio + "\n");
+        }
+        s.close();
+    }
+}
